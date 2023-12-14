388af246dbb2d0f3c45140cbbe5e92d7ef1c5fe3
Pavel.Sukhanov <sukhanov.p.v@yandex.ru>
2023-12-14 12:31:19 +0300

    remove unnessesary files

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 from lib.base_case import BaseCase
+from lib.assertions import Assertions
 
 
 class TestUserAuth(BaseCase):
@@ -9,7 +10,7 @@ class TestUserAuth(BaseCase):
         ("no_token")
     ]
 
-    def setup(self):
+    def setup_method(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
@@ -26,12 +27,12 @@ class TestUserAuth(BaseCase):
         response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
                                  headers={"x-csrf-token": self.token},
                                  cookies={"auth_sid": self.auth_sid})
-
-        assert "user_id" in response2.json(), "There is no user id in the second response"
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert self.user_id_from_auth_method == user_id_from_check_method, \
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
             "User id from auth method is not equal to user id from check method"
+        )
 
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
@@ -41,8 +42,9 @@ class TestUserAuth(BaseCase):
         else:
             response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
                                      cookies={"auth_sid": self.auth_sid})
-        assert "user_id" in response2.json(), "There is no user id in the second response"
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert user_id_from_check_method == 0, \
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
             f"User is authorized with condition {condition}"
+        )
